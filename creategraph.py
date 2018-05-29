a952619b8f2c4b0d9e6c74018f384f48a5bbb76f
Alix Chagué <alix.chague@enc-sorbonne.fr>
2018-05-29 23:26:32 +0200

    màj du script : ajout d'1 vérification des codes précédents pour éviter de répéter un graph ; modification de l'emplacement de calcul d'éléments de style (pour qu'un graph contienne toujours uniquement le même style de forme) ; màj de la récupération du titre de chaque axe

--- creategraph.py
+++ creategraph.py
@@ -6,7 +6,7 @@
 import random
 import plotly.plotly as py
 import plotly.graph_objs as go
-from sample import sample
+from sample import othersample as sample
 from code import previouscodes
 
 ########################################################
@@ -68,16 +68,18 @@ def createDataBar(xData, yData, name, orientation):
     return barTrack
 
 
-def createDataLine(xData, yData, name):
+def createDataLine(xData, yData, name, mode, shape):
     """
     Creates a scatter object for line charts
     :param xData: list
     :param yData: list
     :param name: string
+    :param mode: string
+    :param shape: string
     :return: plotly.graph_objs.graph_objs.Scatter
     """
-    mode = random.choice(['lines', 'lines+markers'])
-    shape = random.choice(['hvh', 'spline', 'linear'])
+    print("DEBUG : createDataLine : mode = " + mode)
+    print("DEBUG : createDataLine : shape = " + shape)
     color = pickColor()
     # creating the scatter object to return
     lineTrack = go.Scatter(
@@ -93,15 +95,16 @@ def createDataLine(xData, yData, name):
     return lineTrack
 
 
-def createDataFill(xData, yData, name):
+def createDataFill(xData, yData, name, mode):
     """
     Creates a scatter object for filled-line charts
     :param xData: list
     :param yData: list
     :param name: string
+    :param mode: string
     :return: plotly.graph_objs.graph_objs.Scatter
     """
-    mode = random.choice(['lines', 'none'])
+    print("DEBUG : createDataFill : mode = " + mode)
     color = pickColor()
     # creating the scatter object to return
     fillTrack = go.Scatter(
@@ -117,18 +120,19 @@ def createDataFill(xData, yData, name):
     return fillTrack
 
 
-def createDataDot(xData, yData, name):
+def createDataDot(xData, yData, name, symbol):
     """
     Creates a scatter object for dot charts
     :param xData: list
     :param yData: list
     :param name: string
+    :param symbol: string
     :return: plotly.graph_objs.graph_objs.Scatter
     """
-    symbol = random.choice(['circle','x','diamond-wide','star-diamond-dot'])
+    print("DEBUG : createDataDot : symbol " + symbol)
     color1 = pickColor()
     color2 = pickColor()
-
+    # creating the scatter object to return
     dotTrack = go.Scatter(
         x=xData,
         y=yData,
@@ -156,6 +160,7 @@ def createDataBubble(xData, yData, zData, name):
     :return: plotly.graph_objs.graph_objs.Scatter
     """
     color = pickColor()
+    # creating the scatter object to return
     bubbleTrack = go.Scatter(
         x=xData,
         y=yData,
@@ -173,7 +178,7 @@ def createDataBubble(xData, yData, zData, name):
     return bubbleTrack
 
 
-def retrieveDataTrack(track, scn, mode, orientation):
+def retrieveDataTrack(track, scn, mode, orientation, dotSymbol, lineShape, lineStyle, fillStyle):
     """
     Calculate data from a sample to create traces for charts
     Call appropriate function depending on the scenario and chart mode indicated
@@ -181,30 +186,42 @@ def retrieveDataTrack(track, scn, mode, orientation):
     :param scn: string
     :param mode: string
     :param orientation: string
+    :param dotSymbol: string
+    :param lineShape: string
+    :param lineStyle: string
+    :param fillStyle: string
     :return: tuple containing plotly.graph_objs.graph_objs.Scatter for data list, then the name of current dataset
     """
     databundle = sample[str(track)]
-    print("DEBUG : retrieveDataTrack() : databundle = ", databundle)
-    xData = databundle['x']
-    print("DEBUG : retrieveDataTrack() : xData = ", xData)
-    yData = databundle['y']
-    print("DEBUG : retrieveDataTrack() : yData = ", yData)
-    zData = databundle['z']
-    print("DEBUG : retrieveDataTrack() : zData = ", zData)
+    xData = databundle['x'][1]
+    yData = databundle['y'][1]
+    yName = databundle['y'][0][0]
+    zData = databundle['z'][1]
+    zName = databundle['z'][0][0]
     trackName = databundle['name']
-    print("DEBUG : retrieveDataTrack() : trackName = ", trackName)
-
+    # Chosing wether the y axis will dwell from yData or zData
+    axis = random.choice(['y','z'])
+    if axis == 'z':
+        rData = zData
+        rName = zName
+    else:
+        rData = yData
+        rName = yName
     if scn == 'barscn':
-        trackData = createDataBar(xData, yData, trackName, orientation)
+        trackData = createDataBar(xData, rData, rName, orientation)
     elif scn == 'scatscn':
         if mode == 'f':
-            trackData = createDataFill(xData, yData, trackName)
+            print("DEBUG : using fillStyle, value = " + fillStyle)
+            trackData = createDataFill(xData, rData, rName, fillStyle)
         elif mode == 'b':
             trackData = createDataBubble(xData, yData, zData, trackName)
         elif mode == 'l':
-            trackData = createDataLine(xData, yData, trackName)
+            print("DEBUG : using lineStyle, value = " + lineStyle)
+            print("DEBUG : using lineShape, value = " + lineShape)
+            trackData = createDataLine(xData, rData, rName, lineStyle, lineShape)
         elif mode == 'd':
-            trackData = createDataDot(xData, yData, trackName)
+            print("DEBUG : using dotSymbol, value = " + dotSymbol)
+            trackData = createDataDot(xData, rData, rName, dotSymbol)
     return trackData, trackName
 
 
@@ -260,28 +277,20 @@ def barChart(tracks, rMode):
     listname = []
     # Choosing orientation
     rOrientation = random.choice(['h', 'v'])
-    print("DEBUG : barChart() : rOrientation = " + rOrientation)
-
     # Creating each Bar object, appended to data list object
     for track in tracks:
-        print("DEBUG : barChart() loop : going to retrieveDataTrack with track, barscn, None and rOrientation in args")
-        DATA = retrieveDataTrack(track, 'barscn', None, rOrientation)
-        data.append(DATA[0])
-        listname.append(DATA[1])
-        print("DEBUG : barChart() : DATA in result = ", DATA)
+        trace, tracename = retrieveDataTrack(track, 'barscn', None, rOrientation, None, None, None, None)
+        data.append(trace)
+        listname.append(tracename)
     # Calculating chart title
     title = createTitle(listname)
-    print("DEBUG : barChart() : title = " + title)
-
     # Creating Layout object
     if rMode == 'g':
         layout = groupLayout(title)
     if rMode == 's':
         layout = stackLayout(title)
-
     # Generating the figure to return
     fig = go.Figure(data=data, layout=layout)
-    print("DEBUG : barChart() : fig : returning fig")
     return fig
 
 
@@ -296,20 +305,24 @@ def scatChart(tracks, rMode):
     :param rMode: string
     :return: plotly.graph_objs.graph_objs.Figure
     """
-    # Chosing scatter chart style
     listname = []
     data = []
+    # Chosing scatter chart style
+    dotSymbol = random.choice(['circle', 'x', 'diamond-wide', 'star-diamond-dot'])
+    print("DEBUG : dotSymbol = " + dotSymbol)
+    lineStyle = random.choice(['lines', 'lines+markers'])
+    print("DEBUG : lineShape = " + lineStyle)
+    lineShape = random.choice(['hvh', 'spline', 'vhv', 'linear', 'hv'])
+    print("DEBUG : lineStyle = " + lineShape)
+    fillStyle = random.choice(['lines', 'none'])
+    print("DEBUG : fillStyle = " + fillStyle)
     # Creating each Bar object, appended to data list object
     for track in tracks:
-        print("DEBUG : scatChart() loop : going to retrieveDataTrack with track, scatscn, rMode and None in args")
-        trace, tracename = retrieveDataTrack(track, 'scatscn', rMode, None)
-        print("DEBUG : scatChart() : trace = ", trace)
-        print("DEBUG : scatChart() : tracename = ", tracename)
+        trace, tracename = retrieveDataTrack(track, 'scatscn', rMode, None, dotSymbol, lineShape, lineStyle, fillStyle)
         data.append(trace)
         listname.append(tracename)
     # Calculating chart title
     title = createTitle(listname)
-    print("DEBUG : scatChart() : title = " + title)
     # Creating Layout object
     layout = go.Layout(
         title=title,
@@ -324,7 +337,6 @@ def scatChart(tracks, rMode):
     )
     # Generating the figure to return
     fig = go.Figure(data=data, layout=layout)
-    print("DEBUG : scatChart() : fig : returning fig")
     return fig
 
 
@@ -341,14 +353,9 @@ def heatmap(track, axis):
     color1 = pickColor()
     color2 = pickColor()
     colorscale = [[0, color1], [1, color2]]
-    print("DEBUG : colorscale = ", colorscale)
-    tData = sample[str(track)][axis]
-    print("DEBUG : tData = ", tData)
+    tData = sample[str(track)][0][axis]
     zData = [tData[:5], tData[5:10], tData[10:]] #----------------- ADAPT TO DATASET LENGTH
-    print("DEBUG : zData = ", zData)
-    title = sample[str(track)]['name']
-    print("DEBUG : title = " + title) #--------------------------- MODIFY TITLE CALCULATION
-
+    title = sample[str(track)]['name'] #-------------------------- MODIFY TITLE CALCULATION
     trace = go.Heatmap(
         z=zData,
         colorscale=colorscale,
@@ -357,9 +364,9 @@ def heatmap(track, axis):
     layout = go.Layout(
         title=title
     )
-
     fig = go.Figure(data=data, layout=layout)
     return fig
+
 
 #------------------------------------------------------#
 #             TRIGER FIG AND CODE CREATION
@@ -376,58 +383,55 @@ def createFig(rKey):
         rTracks = random.randint(2,5)
         # Chosing which tracks will be displayed
         tracks = random.sample(range(1, 9), rTracks)
-        print("DEBUG : rTracks = " + str(rTracks))
-        print("DEBUG : tracks = ", tracks)
         # Chosing a submode (group or stack) for the bar graph
         rMode = random.choice(['g', 's'])
-        print("DEBUG : rMode = " + rMode)
-        print("DEBUG : barChart() with tracks and rMode in args")
         # Generating the figure
         fig = barChart(tracks, rMode)
         # Generating the unique code for the figure
         sTracks = sorted(tracks)
-        print("DEBUG : sTracks = ", sTracks)
         for x in sTracks:
             trackid = trackid + str(x)
         code = "b" + rMode + str(rTracks) + trackid
-        print("DEBUG : code = " + code)
 
     elif rKey == 3 or rKey == 4 or rKey == 5:
         # Chosing how many tracks will be displayed
         rTracks = random.randint(2, 5)
         # Chosing which tracks will be displayed
         tracks = random.sample(range(1, 9), rTracks)
-        print("DEBUG : rTracks = " + str(rTracks))
-        print("DEBUG : tracks = ", tracks)
         # Chosing a submode (line-filed, line, dot, bubble) for the scatter graph
         rMode = random.choice(['f', 'l', 'd', 'b'])
-        print("DEBUG : rMode = " + rMode)
-        print("DEBUG : going to scatChart() with tracks and rMode in args")
         # Generating the figure
         fig = scatChart(tracks, rMode)
         # Generating the unique code for the figure
         sTracks = sorted(tracks)
-        print("DEBUG : sTracks = ", sTracks)
         for x in sTracks:
             trackid = trackid + str(x)
         code = "s" + rMode + str(rTracks) + trackid
-        print("DBUG : code = " + code)
     elif rKey == 6:
         # Chosing which track will be displayed
         rTrack = random.randint(2,9)
-        print("DEBUG : rTrack =" + str(rTrack))
         # Chosing which axis will be displayed
         axis = random.choice(['y','z'])
-        print("DEBUG : axis =" + str(axis))
-        print("DEBUG : going to Heatmap() with rTracks and axis in args")
         # Generating the figure
         fig = heatmap(rTrack, axis)
         # Generating the unique code for the figure
         code = "h" + axis + str(rTrack)
-        print("DEBUG : code = " + code)
     else:
-        print("unexpected error, random key must be out of range")
+        print("ERROR : unexpected error, random key must be out of range")
     return fig, code
+
+def checkcodelist(code):
+    """
+    Verifies if the code generated already exists in the list of previous codes
+    :param code: string
+    :return: integer
+    """
+    check = 1
+    for x in previouscodes:
+        if code == x:
+            check = 0
+            print("DEBUG : code is " + code + " STARTING OVER")
+    return check
 
 
 def writenewcodelist(code):
@@ -448,11 +452,15 @@ def writenewcodelist(code):
 ########################################################
 #                        SCRIPT                        #
 ########################################################
-rKey = random.randint(1, 6)
-print("DEBUG : rKEy = " + str(rKey))
+check = 0
+code = 0
 
-fig, code = createFig(rKey)
-
+while check == 0:
+    check = checkcodelist(code)
+    rKey = random.randint(1, 6)
+    print("DEBUG : rKEy = " + str(rKey))
+    fig, code = createFig(rKey)
+print("DEBUG : final code is " + code)
 writenewcodelist(code)
 filename = "images/" + code
 py.image.save_as(fig, filename=filename, format='png')
